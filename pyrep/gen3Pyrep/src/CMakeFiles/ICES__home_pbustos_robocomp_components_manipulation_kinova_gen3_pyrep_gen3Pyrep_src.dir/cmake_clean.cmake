9ca02c4d839471fddaace98694439ba972d19b58
pbustos <pbustos@unex.es>
2021-12-02 08:48:27 +0100

    borrar morralla

--- pyrep/gen3Pyrep/src/CMakeFiles/ICES__home_pbustos_robocomp_components_manipulation_kinova_gen3_pyrep_gen3Pyrep_src.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ICES__home_pbustos_robocomp_components_manipulation_kinova_gen3_pyrep_gen3Pyrep_src.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
