Rotation fixed && dual camera upgraded

agents/gen3_dsr/src/specificworker.py
@@ -27,11 +27,9 @@ import interfaces as ifaces
 
 import kinovaControl
 
-import multiprocessing as mp
-import time
-import vid_streamv32 as vs
-import cv2
+import pyrealsense2 as rs
 import numpy as np
+import cv2
 
 sys.path.append('/opt/robocomp/lib')
 console = Console(highlight=False)
@@ -56,43 +54,16 @@ class SpecificWorker(GenericWorker):
 
         self.arm = kinovaControl.KinovaGen3()
 
-        #Current Cam
-        self.camProcess = None
-        self.cam_queue = None
-        self.stopbit = None
-        self.camlink = 'rtsp://192.168.1.10/depth' # Add your RTSP cam link
-        self.framerate = 15
+        self.HAND_CAMERA_SN = '839112060624'
 
-        #Current Cam
-        self.colorCamProcess = None
-        self.color_queue = None
-        self.colorStopbit = None
-        self.colorLink = 'rtsp://192.168.1.10/color' # Add your RTSP cam link
-        self.colorFramerate = 15
+        # Hand camera configuration
+        self.pipeline_hand = rs.pipeline()
+        config_hand = rs.config()
+        config_hand.enable_device(self.HAND_CAMERA_SN)
+        config_hand.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+        config_hand.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
-        #set  queue size
-        self.cam_queue = mp.Queue(maxsize=1)
-        self.color_queue = mp.Queue(maxsize=1)
-
-        #get all cams
-        time.sleep(3)
-
-
-        self.stopbit = mp.Event()
-        self.camProcess = vs.StreamCapture( self.camlink,
-                                            self.stopbit,
-                                            self.cam_queue,
-                                            self.framerate)
-
-                            
-        self.camProcess.start()
-        
-        self.colorStopbit = mp.Event()
-        self.colorCamProcess = vs.StreamCapture(self.colorLink,
-                                                self.colorStopbit,
-                                                self.color_queue,
-                                                self.colorFramerate)
-        self.colorCamProcess.start()
+        self.pipeline_hand.start(config_hand)
 
         self.camera_node = self.g.get_node('hand_camera')
         
@@ -105,9 +76,9 @@ class SpecificWorker(GenericWorker):
             self.camera_node.attrs['cam_rgb_focaly'  ] = Attribute (651.855994, self.agent_id)
             self.camera_node.attrs['cam_rgb_cameraID'] = Attribute (0,          self.agent_id)
 
-            self.camera_node.attrs['cam_depth'         ] = Attribute (np.zeros((270,480,3), np.uint8), self.agent_id)
-            self.camera_node.attrs['cam_depth_width'   ] = Attribute (270,           self.agent_id)
-            self.camera_node.attrs['cam_depth_height'  ] = Attribute (480,           self.agent_id)
+            self.camera_node.attrs['cam_depth'         ] = Attribute (np.zeros((480,640,2), np.uint8), self.agent_id)
+            self.camera_node.attrs['cam_depth_width'   ] = Attribute (480,           self.agent_id)
+            self.camera_node.attrs['cam_depth_height'  ] = Attribute (640,           self.agent_id)
             self.camera_node.attrs['cam_depth_depth'   ] = Attribute (2,             self.agent_id)
             self.camera_node.attrs['cam_depth_focalx'  ] = Attribute (360.01333,     self.agent_id)
             self.camera_node.attrs['cam_depth_focaly'  ] = Attribute (360.013366699, self.agent_id)
@@ -118,12 +89,17 @@ class SpecificWorker(GenericWorker):
 
             self.g.update_node(self.camera_node)
 
+        align_to = rs.stream.color
+        self.align = rs.align(align_to)
+
         self.gripper = self.g.get_node('gripper')
         self.gripper.attrs['gripper_finger_distance'] = Attribute (float(0.0), self.agent_id)
         self.gripper.attrs['gripper_target_finger_distance'] = Attribute (float(0.0), self.agent_id)
         self.g.update_node(self.gripper)
 
         self.GRIPPER_ID = self.gripper.id
+
+        self.moving = False
 
 
         try:
@@ -145,34 +121,10 @@ class SpecificWorker(GenericWorker):
 
     def __del__(self):
         """Destructor"""
-        if self.stopbit is not None:
-            self.stopbit.set()
-            while not self.cam_queue.empty():
-                try:
-                    _ = self.cam_queue.get()
-                except:
-                    break
-                self.cam_queue.close()
-
-            self.camProcess.join()
-
-        if self.colorStopbit is not None:
-            self.colorStopbit.set()
-            while not self.color_queue.empty():
-                try:
-                    _ = self.color_queue.get()
-                except:
-                    break
-                self.color_queue.close()
-
-            self.colorCamProcess.join()
+        self.pipeline_hand.stop()
+        
 
     def setParams(self, params):
-        # try:
-        #	self.innermodel = InnerModel(params["InnerModelPath"])
-        # except:
-        #	traceback.print_exc()
-        #	print("Error reading config params")
         return True
 
     def update_gripper_state(self):
@@ -180,7 +132,11 @@ class SpecificWorker(GenericWorker):
         self.gripper = self.g.get_node('gripper')
         gr_state = self.arm.get_gripper_state()            
         self.gripper.attrs['gripper_finger_distance'].value = 1 - gr_state
-        self.g.update_node (self.gripper)
+        if not self.moving:
+            self.g.update_node (self.gripper)
+        else:
+            self.moving = False
+
         
 
     def update_arm_pose(self):
@@ -215,93 +171,69 @@ class SpecificWorker(GenericWorker):
 
     @QtCore.Slot()
     def compute(self):
-        # print('SpecificWorker.compute...')
 
         self.update_arm_pose()
-        # self.update_gripper_state()
+        self.update_gripper_state()
+
+        frames = self.pipeline_hand.wait_for_frames()
+        aligned_frames = self.align.process(frames)
+
+        depth_frame = aligned_frames.get_depth_frame()
+        color_frame = aligned_frames.get_color_frame()
+        if not depth_frame or not color_frame:
+            print ('No hand frame')
+            return True
         
-        depth, color = self.get_rgbd_stream()
+
+         # Convert images to numpy arrays
+        depth_image = np.asanyarray(depth_frame.get_data())
+        color_image = np.asanyarray(color_frame.get_data())
+
+        depth_intrin = depth_frame.profile.as_video_stream_profile().intrinsics
+        color_intrin = color_frame.profile.as_video_stream_profile().intrinsics
+
+        # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
+        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+
+        # images = np.hstack((color_image, depth_colormap))
+        # images = np.hstack((color_image_h, depth_colormap_h))
+
+        # Show images
+        # cv2.namedWindow('RealSense hand', cv2.WINDOW_AUTOSIZE)
+        # cv2.imshow('RealSense hand', images)
+        # cv2.waitKey(1)
 
         # Suboptimal, should treat them independently
-        if color is not None and depth is not None: 
+        if color_image is not None and depth_image is not None: 
             
-            depth = depth.tobytes()
+            depth = depth_image.tobytes()
             depth = np.frombuffer(depth, dtype=np.uint8)
-            depth = depth.reshape((270, 480, 2)) 
+            depth = depth.reshape((480, 640, 2)) 
 
             self.camera_node = self.g.get_node('hand_camera')
 
             if self.camera_node is not None:
-                self.camera_node.attrs['cam_rgb'].value = color
-                self.camera_node.attrs['cam_rgb_height'].value = color.shape[0]
-                self.camera_node.attrs['cam_rgb_width'].value  = color.shape[1]
-                self.camera_node.attrs['cam_rgb_depth'].value  = color.shape[2]
+                self.camera_node.attrs['cam_rgb'].value = cv2.cvtColor(color_image, cv2.COLOR_BGR2RGB)
+                self.camera_node.attrs['cam_rgb_height'].value = color_image.shape[0]
+                self.camera_node.attrs['cam_rgb_width'].value  = color_image.shape[1]
+                self.camera_node.attrs['cam_rgb_depth'].value  = color_image.shape[2]
+                self.camera_node.attrs['cam_rgb_focalx'].value = float(color_intrin.fx)
+                self.camera_node.attrs['cam_rgb_focaly'].value = float(color_intrin.fy)
 
                 self.camera_node.attrs['cam_depth'].value = depth
                 self.camera_node.attrs['cam_depth_height'].value = depth.shape[0]
                 self.camera_node.attrs['cam_depth_width'].value  = depth.shape[1]
                 # self.camera_node.attrs['cam_ed_depth'].value = color.shape[2]
+                self.camera_node.attrs['cam_depth_focalx'].value = float(depth_intrin.fx)
+                self.camera_node.attrs['cam_depth_focaly'].value = float(depth_intrin.fy)
 
                 self.g.update_node(self.camera_node)
-
-                # camera_node.attrs['cam_rgb'].value = color.data
-                # camera_node.attrs['cam_rgb_width'].value = color.cols
-                # camera_node.attrs['cam_rgb_height'].value = color.rows
+        
 
         return True
 
     def startup_check(self):
         QTimer.singleShot(200, QApplication.instance().quit)
-
-    def get_rgbd_stream(self):
-        depth = None 
-        color_color = None
-        if not self.cam_queue.empty():
-            # print('Got frame')
-            cmd, val = self.cam_queue.get()
-
-            # if cmd == vs.StreamCommands.RESOLUTION:
-            #     pass #print(val)
-
-            if cmd == vs.StreamCommands.FRAME:
-                if val is not None:
-                    
-                    # print ("depth: ", val[240][127])
-
-                    # val = val.astype(np.uint8)
-
-                    depth = val # cv2.cvtColor(val, cv2.COLOR_GRAY2RGB)
-
-                    # cv2.imshow('depth', color)
-                    # qt_color = QImage(color, val.shape[1], val.shape[0], QImage.Format_RGB888)
-                    # pix_color = QPixmap.fromImage(qt_color).scaled(self.ui.depth.width(), self.ui.depth.height())
-                    # self.ui.depth.setPixmap(pix_color)
-
-                    # print (val.shape, val)
-
-
-        if not self.color_queue.empty():
-            # print('Got frame')
-            color_cmd, color_val = self.color_queue.get()
-
-            # if cmd == vs.StreamCommands.RESOLUTION:
-            #     pass #print(val)
-
-            if color_cmd == vs.StreamCommands.FRAME:
-                if color_val is not None:
-                    
-                    # print ("img: ", val.shape[1], val.shape[0], " UI: ", self.ui.color.width(), self.ui.color.height() )
-
-                    color_color = cv2.cvtColor(color_val, cv2.COLOR_BGR2RGB)
-                    # color_qt_color = QImage(color_color, color_val.shape[1], color_val.shape[0], QImage.Format_RGB888)
-                    # color_pix_color = QPixmap.fromImage(color_qt_color).scaled(self.ui.color.width(), self.ui.color.height())
-                    # self.ui.color.setPixmap(color_pix_color)
-                    # cv2.imshow('color', color_color)
-
-                    # print (val.shape, val)
-
-        return depth, color_color
-
 
 
     # =============== DSR SLOTS  ================
@@ -311,6 +243,7 @@ class SpecificWorker(GenericWorker):
         # console.print(f"UPDATE NODE ATT: {id} {attribute_names}", style='green')
 
         if id == self.GRIPPER_ID and 'target' in attribute_names:
+            self.moving = True
             updated_node = self.g.get_node(id)
             target_position  = updated_node.attrs['target'].value
             print ("Received target position", target_position)

agents/gen3_mission_control/src/specificworker.py
@@ -26,6 +26,7 @@ from rich.console import Console
 from genericworker import *
 import interfaces as ifaces
 
+import pyrealsense2 as rs
 import numpy as np
 import cv2
 
@@ -57,7 +58,6 @@ class SpecificWorker(GenericWorker):
 
         self.CUBE_PREFIX = 1000
 
-        self.tf = inner_api(self.g)
         self.tag_detection_count = {}
         self.tags = {}
         self.cube_pos = {}
@@ -66,6 +66,9 @@ class SpecificWorker(GenericWorker):
             on_press=self.on_press,
             on_release=self.on_release)
         listener.start()
+
+        self.align_to = rs.stream.color
+        self.align = rs.align(self.align_to)
 
         try:
             signals.connect(self.g, signals.UPDATE_NODE_ATTR, self.update_node_att)
@@ -146,6 +149,15 @@ class SpecificWorker(GenericWorker):
         dest_pose[3] = 0.0
         dest_pose[4] = np.pi
 
+        # -45 para minizar giro, + 90 para cuadrar con el gripper
+        dest_pose[5] = np.degrees(dest_pose[5]) % 90
+        if dest_pose[5] < 45:
+            dest_pose[5] += 90
+
+        dest_pose[5] -= 45
+        dest_pose[5] = (90 - dest_pose[5]) % 90 + 45
+        dest_pose[5] = np.radians(dest_pose[5])
+
         print (dest_pose)
         gripper.attrs["gripper_target_finger_distance"].value = 1.0
         gripper.attrs["target"].value = dest_pose
@@ -154,8 +166,8 @@ class SpecificWorker(GenericWorker):
     def insert_or_update_cube (self, cube_id):
         cube = self.tags[cube_id]
        
-        
-        new_pos = self.tf.transform_axis ("world", cube["pos"] + cube["rot"], "hand_camera")
+        tf = inner_api(self.g)
+        new_pos = tf.transform_axis ("world", cube["pos"] + cube["rot"], "hand_camera")
 
         if (cube_node := self.g.get_node("cube_" + str(cube_id))) is None:
             new_node = Node(cube_id + self.CUBE_PREFIX, "box", "cube_" + str(cube_id))
@@ -165,9 +177,9 @@ class SpecificWorker(GenericWorker):
         
         # print ("Inserted cube " + str(cube_id))
 
+        rt = rt_api(self.g)
         world = self.g.get_node ("world")
-        self.rt = rt_api(self.g)
-        self.rt.insert_or_assign_edge_RT(world, cube_node.id, new_pos[:3], new_pos[3:])
+        rt.insert_or_assign_edge_RT(world, cube_node.id, new_pos[:3], new_pos[3:])
         self.g.update_node(world)
 
     def delete_cube (self, cube_id):
@@ -180,7 +192,7 @@ class SpecificWorker(GenericWorker):
             # print (type(self.depth), self.depth.shape)
 
             self.depth = np.frombuffer(self.depth_raw, dtype=np.uint16)
-            self.depth = self.depth.reshape((270, 480))
+            self.depth = self.depth.reshape((480, 640))
 
             # depth[ depth > 2000] = 0
             # print (depth.dtype, depth[135, 240])
@@ -191,8 +203,9 @@ class SpecificWorker(GenericWorker):
 
             self.color = np.frombuffer(self.color_raw, dtype=np.uint8)
             self.color = self.color.reshape((480, 640, 3))
-            self.inmediate_tags = self.compute_april_tags()
+            self.color = cv2.resize(self.color, (1280, 960), interpolation=cv2.INTER_NEAREST)
 
+            self.inmediate_tags = self.compute_april_tags()
             for id in self.inmediate_tags.keys():
                 if id not in self.tag_detection_count.keys():
                     self.tag_detection_count[id] = 0
@@ -209,8 +222,10 @@ class SpecificWorker(GenericWorker):
                 else:
                     self.delete_cube (id)
 
-            
-            cv2.imshow('Color', cv2.cvtColor(self.depth.astype(np.uint8), cv2.COLOR_RGB2BGR)) #depth.astype(np.uint8))
+            dept_show = cv2.applyColorMap(cv2.convertScaleAbs(self.depth, alpha=0.03), cv2.COLORMAP_JET)
+            # dept_show = cv2.rectangle (dept_show, (450, 268), (118, 81), (255, 255, 255))  
+            # dept_show = cv2.resize(dept_show, dsize=(1280, 720))          
+            cv2.imshow('Color', self.color) #depth.astype(np.uint8))
 
         return True
 
@@ -228,7 +243,7 @@ class SpecificWorker(GenericWorker):
     def update_node(self, id: int, type: str):
         # console.print(f"UPDATE NODE: {id} {type}", style='green')
 
-        if type=='rgbd':
+        if type=='rgbd' and id == 62842907933016084:
             updated_node = self.g.get_node(id)
             self.depth_raw = updated_node.attrs['cam_depth'].value
             self.color_raw = updated_node.attrs['cam_rgb'].value
@@ -256,7 +271,6 @@ class SpecificWorker(GenericWorker):
         tags = self.detector.detect(cv2.cvtColor(self.color, cv2.COLOR_RGB2GRAY))
         if len(tags) > 0:
             for tag in tags:
-                print (tag)
                 for idx in range(len(tag.corners)):
                     cv2.line(self.color, tuple(tag.corners[idx - 1, :].astype(int)), tuple(tag.corners[idx, :].astype(int)), (0, 255, 0))
                     cv2.putText(self.color, str(tag.tag_id),
@@ -276,23 +290,32 @@ class SpecificWorker(GenericWorker):
         for tag in tags:
             # print (tag.center)
             # print (int(tag.center[1] / 1.77), int(tag.center[0] / 1.33))
-            index_x = int(tag.center[1] / 1.91 + 10 )
-            index_y = int(tag.center[0] / 1.96 + 100)
-            pos_z = self.depth[index_x][index_y]
-            pos_x = - ((tag.center[1] - self.color.shape[0] // 2) * pos_z) / self.focal_x  
-            pos_y = - ((tag.center[0] - self.color.shape[1] // 2) * pos_z) / self.focal_y
+            index_x = int(tag.center[1]) // 2
+            index_y = int(tag.center[0]) // 2
+            pos_z = np.mean(self.depth[index_x-10:index_x+10,index_y-10:index_y+10])
+
+
+            pos_x = - ((tag.center[1]//2 - self.color.shape[0] // 4) * pos_z) / self.focal_x  
+            pos_y = - ((tag.center[0]//2 - self.color.shape[1] // 4) * pos_z) / self.focal_y
 
             pos = [pos_y, pos_x, pos_z] # Acording to gripper reference frame
+            # pos = [pos_x, pos_y, pos_z] # Acording to world reference frame
 
             r_x = 0
             r_y = 0
             r_z = (np.arctan2(tag.corners[0][1] - tag.center[1], tag.corners[0][0] - tag.center[0]) + ( 3 * np.pi )/4) # % 360
+            # print (tag.center[0] - self.color.shape[1] // 2,  tag.center[1] - self.color.shape[0] // 2)
             ori = [r_x, r_y, r_z]
+
+            # print (pos)
+
 
             s_tags[tag.tag_id] = {"pos": pos, "rot": ori}
 
-            cv2.drawMarker(self.depth, (int((tag.center[0]) / 1.96 + 100), int(tag.center[1] / 1.91 + 10)), (0, 255, 0), cv2.MARKER_CROSS, 150, 1)
+            # cv2.drawMarker(self.depth, (int(tag.center[0]), int(tag.center[1])), (0, 255, 0), cv2.MARKER_CROSS, 150, 1)
+            # cv2.drawMarker(self.depth, (int(tag.corners[0][0]), int(tag.corners[0][1])), (0, 255, 0), cv2.MARKER_CROSS, 150, 1)
+            # cv2.drawMarker(self.depth, (int(tag.corners[1][0]), int(tag.corners[2][1])), (0, 255, 0), cv2.MARKER_CROSS, 150, 1)
 
-            self.depth = cv2.rectangle (self.depth, (95, 15), (430, 260), (0, 255, 0))
+            # self.depth = cv2.rectangle (self.depth, (300, 220), (340, 260), (0, 255, 0))
         return s_tags
         

agents/real_sense_pub/src/specificworker.py
@@ -19,13 +19,14 @@
 #    along with RoboComp.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-from turtle import color
+# from turtle import color
 from PySide2.QtCore import QTimer
 from PySide2.QtWidgets import QApplication
 from rich.console import Console
 from genericworker import *
 import interfaces as ifaces
 
+import pyrealsense2 as rs
 import numpy as np
 import cv2
 
@@ -44,19 +45,66 @@ from pydsr import *
 class SpecificWorker(GenericWorker):
     def __init__(self, proxy_map, startup_check=False):
         super(SpecificWorker, self).__init__(proxy_map)
-        self.Period = 2000
+        self.Period = 100
 
         # YOU MUST SET AN UNIQUE ID FOR THIS AGENT IN YOUR DEPLOYMENT. "_CHANGE_THIS_ID_" for a valid unique integer
-        self.agent_id = "_CHANGE_THIS_ID_"
-        self.g = DSRGraph(0, "pythonAgent", self.agent_id)
+        self.agent_id = 1123
+        self.g = DSRGraph(0, "real_sense_pub", self.agent_id)
+        self.rt = rt_api(self.g)
+
+        self.TOP_CAMERA_SN = '151422251126'
+
+        
+        self.camera_node = self.g.get_node('top_view_camera')
+        if self.camera_node is not None: # TODO: Edit camera params
+            self.camera_node.attrs['cam_rgb'         ] = Attribute (np.zeros((480,640,3), np.uint8), self.agent_id)
+            self.camera_node.attrs['cam_rgb_width'   ] = Attribute (640,        self.agent_id)
+            self.camera_node.attrs['cam_rgb_height'  ] = Attribute (480,        self.agent_id)
+            self.camera_node.attrs['cam_rgb_depth'   ] = Attribute (3,          self.agent_id)
+            self.camera_node.attrs['cam_rgb_focalx'  ] = Attribute (653.68229,  self.agent_id)
+            self.camera_node.attrs['cam_rgb_focaly'  ] = Attribute (651.855994, self.agent_id)
+            self.camera_node.attrs['cam_rgb_cameraID'] = Attribute (0,          self.agent_id)
+
+            self.camera_node.attrs['cam_depth'         ] = Attribute (np.zeros((480,640,2), np.uint8), self.agent_id)
+            self.camera_node.attrs['cam_depth_width'   ] = Attribute (640,           self.agent_id)
+            self.camera_node.attrs['cam_depth_height'  ] = Attribute (480,           self.agent_id)
+            self.camera_node.attrs['cam_depth_depth'   ] = Attribute (2,             self.agent_id)
+            self.camera_node.attrs['cam_depth_focalx'  ] = Attribute (360.01333,     self.agent_id)
+            self.camera_node.attrs['cam_depth_focaly'  ] = Attribute (360.013366699, self.agent_id)
+            self.camera_node.attrs['cam_depthFactor'   ] = Attribute (0.01,          self.agent_id)
+            self.camera_node.attrs['cam_depth_cameraID'] = Attribute (1,             self.agent_id)
+
+            self.g.update_node(self.camera_node)
+        
+            # TODO: Edit camera RT
+            # world   = self.g.get_node('world')
+            # camera_pos = [0, 0, 0]
+            # camera_rot = [0, 0, 0]
+            # new_pos = [self.camera_node.id, camera_pos, camera_rot]
+            # self.rt.insert_or_assign_edge_RT(world, *new_pos)
+            # self.g.update_node(world)
+        
+
+        # Real Sense generic configuration
+        # Top camera configuration
+        self.pipeline_top = rs.pipeline()
+        config_top = rs.config()
+        config_top.enable_device(self.TOP_CAMERA_SN)
+        config_top.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+        config_top.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+        self.pipeline_top.start(config_top)
+
+        align_to = rs.stream.color
+        self.align = rs.align(align_to)
 
         try:
-            signals.connect(self.g, signals.UPDATE_NODE_ATTR, self.update_node_att)
-            signals.connect(self.g, signals.UPDATE_NODE, self.update_node)
-            signals.connect(self.g, signals.DELETE_NODE, self.delete_node)
-            signals.connect(self.g, signals.UPDATE_EDGE, self.update_edge)
-            signals.connect(self.g, signals.UPDATE_EDGE_ATTR, self.update_edge_att)
-            signals.connect(self.g, signals.DELETE_EDGE, self.delete_edge)
+            # signals.connect(self.g, signals.UPDATE_NODE_ATTR, self.update_node_att)
+            # signals.connect(self.g, signals.UPDATE_NODE, self.update_node)
+            # signals.connect(self.g, signals.DELETE_NODE, self.delete_node)
+            # signals.connect(self.g, signals.UPDATE_EDGE, self.update_edge)
+            # signals.connect(self.g, signals.UPDATE_EDGE_ATTR, self.update_edge_att)
+            # signals.connect(self.g, signals.DELETE_EDGE, self.delete_edge)
             console.print("signals connected")
         except RuntimeError as e:
             print(e)
@@ -69,6 +117,7 @@ class SpecificWorker(GenericWorker):
 
     def __del__(self):
         """Destructor"""
+        self.pipeline_top.stop()
 
     def setParams(self, params):
         return True
@@ -76,15 +125,62 @@ class SpecificWorker(GenericWorker):
 
     @QtCore.Slot()
     def compute(self):
-        print('SpecificWorker.compute...')
+
+        # Wait for a coherent pair of frames: depth and color
+        frames = self.pipeline_top.wait_for_frames()
+
+        aligned_frames = self.align.process(frames)
+
+        depth_frame = aligned_frames.get_depth_frame()
+        color_frame = aligned_frames.get_color_frame()
+
+        if not depth_frame or not color_frame:
+            print ('No top frame')
+            return True
         
-        all_raw, color_raw, dept_rawh = self.camerargbdsimple_proxy.getAll()
-        color = np.frombuffer(color_raw.image, np.uint8).reshape(color_raw.width, color_raw.height, color_raw.depth)
-        color = cv2.cvtColor(color, cv2.COLOR_BGR2RGB)
+         # Convert images to numpy arrays
+        depth_image = np.asanyarray(depth_frame.get_data())
+        color_image = np.asanyarray(color_frame.get_data())
 
-        cv2.imshow("COLOR", color)
-        cv2.waitKey(1)
+        depth_intrin = depth_frame.profile.as_video_stream_profile().intrinsics
+        color_intrin = color_frame.profile.as_video_stream_profile().intrinsics
+        
+        # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
+        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
 
+        # images = np.hstack((color_image, depth_colormap))
+        # images = np.hstack((color_image_h, depth_colormap_h))
+
+        # Show images
+        # cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
+        # cv2.imshow('RealSense', images)
+        # cv2.waitKey(1)
+
+        # Suboptimal, should treat them independently
+        if color_image is not None and depth_image is not None: 
+            
+            depth = depth_image.tobytes()
+            depth = np.frombuffer(depth, dtype=np.uint8)
+            depth = depth.reshape((480, 640, 2)) 
+
+            self.camera_node = self.g.get_node('top_view_camera')
+
+            if self.camera_node is not None:
+                self.camera_node.attrs['cam_rgb'].value = cv2.cvtColor(color_image, cv2.COLOR_BGR2RGB)
+                self.camera_node.attrs['cam_rgb_height'].value = color_image.shape[0]
+                self.camera_node.attrs['cam_rgb_width' ].value  = color_image.shape[1]
+                self.camera_node.attrs['cam_rgb_depth' ].value  = color_image.shape[2]
+                self.camera_node.attrs['cam_rgb_focalx'].value = float(color_intrin.fx)
+                self.camera_node.attrs['cam_rgb_focaly'].value = float(color_intrin.fy)
+
+                self.camera_node.attrs['cam_depth'].value = depth
+                self.camera_node.attrs['cam_depth_height'].value = depth.shape[0]
+                self.camera_node.attrs['cam_depth_width' ].value  = depth.shape[1]
+                # self.camera_node.attrs['cam_ed_depth'].value = color.shape[2]
+                self.camera_node.attrs['cam_depth_focalx'].value = float(depth_intrin.fx)
+                self.camera_node.attrs['cam_depth_focaly'].value = float(depth_intrin.fy)
+
+                self.g.update_node(self.camera_node)
         return True
 
     def startup_check(self):